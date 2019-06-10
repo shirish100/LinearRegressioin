69ceb3d833d2799402f150a626c7af218cf4507c
shirish100 <sranoji100@gmail.com>
2019-06-10 12:36:00 +0530

    This First Git Code

---
+++ gradientdescent.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import datasets, linear_model
+from sklearn.metrics import mean_squared_error, r2_score
+
+# Load the diabetes dataset
+diabetes = datasets.load_diabetes()
